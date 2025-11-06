0f8b72b95f4cb9a5d22ef9994095afbfdfb6b8c6
Macario Ortega <maca@aelita.io>
2025-11-06 17:16:26 +0100

    Example configured to store jwt to local store

--- example/src/Main.elm
+++ example/src/Main.elm
@@ -1,8 +1,11 @@
-module Main exposing (main)
+port module Main exposing (main)
 
 import PostgRestAdmin
 import PostgRestAdmin.Config as Config
 import PostgRestAdmin.Config.FormAuth as FormAuth
+
+
+port gotToken : String -> Cmd msg
 
 
 main : PostgRestAdmin.Program Never Never Never
@@ -13,4 +16,5 @@ main =
             (FormAuth.config
                 |> FormAuth.authUrl "http://localhost:9080/rpc/login"
             )
+        |> Config.onLogin gotToken
         |> PostgRestAdmin.application
