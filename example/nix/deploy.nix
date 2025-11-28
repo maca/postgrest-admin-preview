27d90a25f975179b48e5fc76a063bd7e096564c6
Macario Ortega <git@aelita.io>
2025-11-28 15:56:20 +0100

    Fix nginx 502

--- example/nix/deploy.nix
+++ example/nix/deploy.nix
@@ -241,7 +241,7 @@ in
         };
 
         locations."/api/" = {
-          proxyPass = "http://unix:${postgrestSocket}/";
+          proxyPass = "http://unix:${postgrestSocket}:/";
           extraConfig = ''
             proxy_set_header Host $host;
             proxy_set_header X-Real-IP $remote_addr;
